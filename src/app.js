cb0b77c91b4a9e91c395569ba6f9cd48e7a97ab6
Guilherme Zanini Moreira <guilherme.zm@outlook.com>
2021-04-13 15:28:24 -0300

    added execrption handler

--- src/app.js
+++ src/app.js
@@ -4,29 +4,40 @@ import mongoose from 'mongoose';
 import routes from './routes';
 import 'dotenv/config';
 
+import AppError from './shared/Errors/AppError';
 class App {
   constructor () {
     this.express = express();
     this.middlewares();
     this.database();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
-    this.express.use(express.json())
-    this.express.use(cors())
+    this.express.use(express.json());
+    this.express.use(cors());
   }
 
   database() {
     mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    })
+    });
   }
 
   routes() {
-    this.express.use(routes)
+    this.express.use(routes);
   }
+
+  exceptionHandler() {
+    this.express.use((err, req, res, next) => {
+       if (err instanceof AppError) {
+          return res.status(err.statusCode).json({ status: 'error', error: err.message });
+       }
+       return res.status(500).json({ status: 'error', message: 'Internal server error!' });
+    });
+ }
 }
 
 export default new App().express
